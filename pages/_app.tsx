4ce0c85331ffad77e87208771eefcae48f3acf82
Peter Nguyen <peternguyenforwork@gmail.com>
2022-09-05 06:21:21 -0700

    update the website icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,17 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
 import '../styles/globals.css'
 import ContainerBlock from '../components/ContainerBlock'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider defaultTheme="system" attribute="class">
+      <Head>
+        <title>Petercrackthecode</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Peter Nguyen's personal website" />
+        <link rel="icon" href="/portfolio_image.ico" />
+      </Head>
       <ContainerBlock>
         <Component {...pageProps} />
       </ContainerBlock>
