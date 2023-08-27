ec616275d91f1e12447b3cc206ad4ee51e6b9c86
Peter Nguyen <peternguyenforwork@gmail.com>
2023-08-26 23:09:38 -0700

    updated my location & "Favourite Projects" to "Favorite Projects"

--- components/FavoriteProjects.tsx
+++ components/FavoriteProjects.tsx
@@ -6,7 +6,7 @@ export default function FavoriteProjects() {
       <div className="max-w-6xl mx-auto">
         <header className="flex flex-col md:flex-row justify-between items-center pt-40 mx-10 md:my-20 lg:my-0">
           <h1 className="text-6xl lg:text-9xl max-w-lg font-bold text-gray-500 my-20 md:my-0 md:text-white dark:text-gray-600 text-center">
-            Favourite Projects
+            Favorite Projects
           </h1>
           <Link href="/projects">
             <a className="mb-20 md:mb-0 px-8 py-4 rounded-md bg-white shadow-lg text-xl font-semibold flex flex-row space-x-4 items-center dark:text-gray-700">
