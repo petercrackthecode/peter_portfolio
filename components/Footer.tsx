e6bba634bc7a755a03eda824db512c2a2d934935
Peter Nguyen <peternguyenforwork@gmail.com>
2024-09-01 21:21:00 -0700

    fix: replaced the broken url at the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
         <div className="inline-block transform hover:scale-110 hover:-rotate-3 transition duration-300">
           <a
             className="hover:bg-red-500 rounded-md px-2 py-1  hover:text-gray-50"
-            href="https://petercrackthecode.com"
+            href="https://peternguyen.io"
             target="_blank"
             rel="noopener noreferrer"
           >
